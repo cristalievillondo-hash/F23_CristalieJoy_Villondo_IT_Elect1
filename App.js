b81b1986caf9ce226b38f01c242a896696dda238
cristalievillondo-hash <cristalievillondo@gmail.com>
2025-09-19 19:08:45 +0800

    SActivity5

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
+<<<<<<< HEAD
 import Comment from './Comment';
 import Messenger from './Messenger';
 import React from 'react';
+=======
+import CounterApp from './CounterApp';
+import ColorChanger from './ColorChangerApp';
+>>>>>>> 1ac8aa3 (Activity5)
 
 export default function App() {
   return (
     <>
+<<<<<<< HEAD
       <Messenger/>
       <Comment/>
     </>
   )
+=======
+    <ColorChanger/>
+    <CounterApp/>
+    </>
+  );
+>>>>>>> 1ac8aa3 (Activity5)
 }
